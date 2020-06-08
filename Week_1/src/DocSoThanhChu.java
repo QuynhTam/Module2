3e26d7cce180f0178f7c3171f03a391e73b1df5d
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-09 00:23:01 +0700

    [Bài tập] Ứng dụng chuyển đổi tiền tệ

---
+++ Week_1/src/DocSoThanhChu.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DocSoThanhChu {
+    public static void main(String[] args) {
+        String number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào 1 số ");
+        number = sc.nextLine() ;
+        System.out.println(number.length());
+    }
+}
