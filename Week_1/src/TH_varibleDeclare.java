509de1a7b138e8095242eb1b5b56f6cf6a093a70
unknown <ngocquynh.ngoctam@gmail.com>
2020-06-08 21:49:22 +0700

    [Thực hành] Khai báo và sử dụng biến

---
+++ Week_1/src/TH_varibleDeclare.java
@@ -0,0 +1,16 @@
+public class TH_varibleDeclare {
+    public static void main(String[] args) {
+        int i=10;
+        float f=20.5f;
+        double d=20.5d;
+        boolean b= true;
+        char a= 'a';
+        String s= "Hà Nội";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(s);
+    }
+}
