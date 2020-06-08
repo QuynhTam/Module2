9a0f28021e8a9f980ccd1f26bcde87257857d6e5
unknown <ngocquynh.ngoctam@gmail.com>
2020-06-08 20:46:59 +0700

    [Thực hành] Hiển thị thời gian của hệ thống

---
+++ Week_1/src/TH_displayTime.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class TH_displayTime {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println("Now is "+ now);
+    }
+}
