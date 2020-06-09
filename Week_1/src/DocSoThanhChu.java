0aa6162ef813dc491ddf59e49bfaddc271c1616a
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-09 11:57:42 +0700

    [Thực hành] Kiểm tra số nguyên tố

--- Week_1/src/DocSoThanhChu.java
+++ Week_1/src/DocSoThanhChu.java
@@ -2,10 +2,26 @@ import java.util.Scanner;
 
 public class DocSoThanhChu {
     public static void main(String[] args) {
-        String number;
+        int number;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào 1 số ");
-        number = sc.nextLine() ;
-        System.out.println(number.length());
+        System.out.println("Nhập vào số cần kiểm tra :");
+        number = sc.nextInt();
+        System.out.println(number + " Là số nguyên tố ? " + isPrimeNumber(number));
+
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        boolean check = true;
+        if (n < 2)
+            check = false;
+        else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+        }
+            return check;
     }
 }
