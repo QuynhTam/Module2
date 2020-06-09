c6fcaa9367b7375fdb33b51384e37f213dc13d57
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-09 14:35:50 +0700

    [Bài tập] Hiển thị các loại hình

---
+++ Week_1/src/HienThiCacLoaiHinh.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class HienThiCacLoaiHinh {
+    public static void main(String[] args) {
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1.tam giác vuông, có cạnh góc vuông ở botton-left");
+        System.out.println("2.tam giác vuông, có cạnh góc vuông ở top-left");
+        System.out.println("3.Hình chữ nhật");
+        System.out.println("0.Exit");
+        System.out.println("Nhập lựa chọn của bạn");
+        choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        System.out.printf("*");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 2:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = i; j < 5; j++) {
+                        System.out.printf("*");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 3:
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        System.out.printf(" * ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("Nhập không hợp lệ");
+        }
+    }
+}
