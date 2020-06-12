e2cbdfb0de3656f9006fe4ed63c93250ffa375cc
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-12 15:52:46 +0700

    [Thực hành] Static Property

---
+++ Week_1/src/CodeGym/Thuchanh2/Car.java
@@ -0,0 +1,14 @@
+package CodeGym.Thuchanh2;
+
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCar;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCar++;
+    }
+
+}
---
+++ Week_1/src/CodeGym/Thuchanh2/Execute.java
@@ -0,0 +1,10 @@
+package CodeGym.Thuchanh2;
+
+public class Execute {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3", "Sky0activ 3");
+        System.out.println(Car.numberOfCar);
+        Car car2 = new Car("Mazda 6", "Skyactiv 6");
+        System.out.println(Car.numberOfCar);
+    }
+}
