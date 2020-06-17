fd4b518183599e6a0b2b4fb8df6902f42879d584
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-17 16:22:59 +0700

    [Thực hành] Refactoring - tách biến

---
+++ Week_1/src/CleanCode/Refactoring/Refactoring.java
@@ -0,0 +1,24 @@
+package CleanCode.Refactoring;
+
+public class Refactoring {
+    public static String fizzBuzz(int number) {
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+
+        if (isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if (isFizz)
+            return "Fizz";
+
+        if (isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+
+    public static void main(String[] args) {
+        String result = fizzBuzz(3);
+        System.out.println(result);
+    }
+}
