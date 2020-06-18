133b7210cedfe50f50945dc569b2ea9ff201d242
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-18 15:17:59 +0700

    [Thực hành] Tìm giá trị tuyệt đối

---
+++ Week_1/src/Demo/Main.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=2;
+        float a=x;
+        float y=3.2f;
+        int z=(int)y;
+        System.out.println(z);
+    }
+}
