798cb3ff51e5af91c413fb24918a7e146feb2e6c
NguyenNgocQuynh <ngocquynh.ngoctam@gmail.com>
2020-06-22 00:06:25 +0700

    [Thực hành] Triển khai Queue sử dụng danh sách liên kết

---
+++ Week_3/src/Queue/MyLinkedListQueue.java
@@ -0,0 +1,34 @@
+package Queue;
+
+public class MyLinkedListQueue {
+    private Node head;
+    private Node tail;
+
+    public MyLinkedListQueue() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void enqueue(int key) {
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.head = this.tail = temp;
+        } else {
+            this.tail.next = temp;
+            this.tail = temp;
+        }
+    }
+
+    public Node dequeue() {
+        if (this.tail == null) {
+            return null;
+        }
+        Node temp = this.head.next;
+        this.head = this.head.next;
+        if (this.head == null) {
+            this.tail = null;
+        }
+        return temp;
+
+    }
+}
