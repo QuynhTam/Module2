9aa29a6a33973e39adc901e8d14326e86a1e3dcd
NguyenNgocQuynh <ngocquynh.ngoctam@gmail.com>
2020-06-27 14:47:19 +0700

    [Bài tập] Sử dụng lớp IllegalTriangleException

---
+++ Week_3/src/Try_Catch/TriangleException/IllegalTriangleException.java
@@ -0,0 +1,10 @@
+package Try_Catch.TriangleException;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException() {
+    }
+
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
